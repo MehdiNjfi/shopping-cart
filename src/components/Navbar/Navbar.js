bcba4d5f295e5c98c941f7219722c8f5ee006c13
Mehdi Najafi <mehdinajafi20.mn@gmail.com>
2020-09-22 21:15:44 +0330

    Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import cartIcon from '../../assets/images/icons/cart.svg'
+import './Navbar.scss'
 
 const Navbar = () => {
   return (
     <header>
-      <nav>Navbar</nav>
+      <nav className="navbar">
+        <ul className="navbar-list">
+          <li className="nav-item nav-logo">Shopping Cart</li>
+          <li className="nav-item nav-cart-logo">
+            <img src={cartIcon} alt="Cart" width="22px" height="22px" />
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
