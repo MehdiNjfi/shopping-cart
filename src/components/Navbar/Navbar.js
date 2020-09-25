5a0c2b83a20ff9b3791c820d2e441d602ae0f5f3
Mehdi Najafi <mehdinajafi20.mn@gmail.com>
2020-09-26 00:03:37 +0330

    Show cart length in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,16 +3,18 @@ import cartIcon from '../../assets/images/icons/cart.svg'
 import './Navbar.scss'
 import { connect } from 'react-redux'
 import { setShowCart } from '../../redux/actions/actions'
+import { getCartProducts } from '../../redux/reducers'
 
-const Navbar = ({ setShowCart }) => {
+const Navbar = ({ cart, setShowCart }) => {
   return (
     <header>
       <nav className="navbar">
         <ul className="navbar-list">
           <li className="nav-item nav-logo">Shopping Cart</li>
           {/* setShowCart action is connected to the Redax Store for show or hide cart*/}
-          <li className="nav-item nav-cart-logo" onClick={setShowCart}>
+          <li className="nav-item nav-cart-icon" onClick={setShowCart}>
             <img src={cartIcon} alt="Cart" width="22px" height="22px" />
+            <span className="cart-length">{cart.length}</span>
           </li>
         </ul>
       </nav>
@@ -20,4 +22,8 @@ const Navbar = ({ setShowCart }) => {
   )
 }
 
-export default connect(null, { setShowCart })(Navbar)
+const mapStateToProps = (state) => {
+  return { cart: getCartProducts(state) }
+}
+
+export default connect(mapStateToProps, { setShowCart })(Navbar)
